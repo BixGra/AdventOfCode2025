fff74e4455ca110d5989c95eb96b2339cff1d693
Bixente Grandjean <bixente.grandjean@gmail.com>
2025-12-08 19:38:20 +0100

    [+] Day 8

--- 08/08.py
+++ 08/08.py
@@ -23,53 +23,16 @@ dists = {
 }
 sorted_dists = list(map(lambda d: d[0], sorted(dists.items(), key=lambda x: x[1])))
 
-
-# Part 1
-
-
-pairs = 1000
-connected_boxes: dict[int, int] = {}
-circuits: dict[int, set] = {}
-
-for i in range(pairs):
-    u, v = sorted_dists[i]
-    match (u in connected_boxes, vseen := v in connected_boxes):
-        case (True, True):
-            if v in circuits[connected_boxes[u]]:
-                continue
-            ucircuit = connected_boxes[u]
-            vcircuit = connected_boxes[v]
-            for box in circuits[vcircuit]:
-                connected_boxes[box] = connected_boxes[ucircuit]
-            circuits[ucircuit] |= circuits[vcircuit]
-            del circuits[vcircuit]
-        case (True, False):
-            ucircuit = connected_boxes[u]
-            connected_boxes[v] = connected_boxes[ucircuit]
-            circuits[ucircuit].add(v)
-        case (False, True):
-            vcircuit = connected_boxes[v]
-            connected_boxes[u] = connected_boxes[vcircuit]
-            circuits[vcircuit].add(u)
-        case _:
-            connected_boxes[u] = u
-            connected_boxes[v] = u
-            circuits[u] = {u, v}
-
-total = prod(sorted(map(len, circuits.values()), reverse=True)[:3])
-print(total)
-
-
-# Part 2
-
-
 connected_boxes: dict[int, int] = {}
 circuits: dict[int, set] = {}
 size = len(boxes)
 
 i = 0
+pairs = 1000
 condition = False
 while not condition:
+    if i == pairs:
+        total1 = prod(sorted(map(len, circuits.values()), reverse=True)[:3])
     u, v = sorted_dists[i]
     i += 1
     match (u in connected_boxes, v in connected_boxes):
@@ -96,7 +59,7 @@ while not condition:
             circuits[u] = {u, v}
     condition = len(list(circuits.values())[0]) == size
 
-total = boxes[u][0] * boxes[v][0]
-print(total)
+total2 = boxes[u][0] * boxes[v][0]
 
-# Part 1 & 2 can easily be merged with an early total value assign after pairs steps
+print(total1)
+print(total2)
