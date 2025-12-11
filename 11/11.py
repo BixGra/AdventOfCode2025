3def58e7f291828811c2657a914e725db5c7765f
Bixente Grandjean <bixente.grandjean@gmail.com>
2025-12-11 15:51:43 +0100

    [+] Day 11

--- 11/11.py
+++ 11/11.py
@@ -1,2 +1,29 @@
+from functools import lru_cache
+
 with open("./11/input.txt", "r") as file:
     lines = file.readlines()
+    connections = {line[:3]: {l for l in line[4:].strip().split(" ")} for line in lines}
+    connections |= {"out": set()}
+
+
+@lru_cache
+def dfs(start: str, end: str) -> int:
+    if start == end:
+        return 1
+    return sum(dfs(server, end) for server in connections[start])
+
+
+# Part 1
+
+
+total = dfs("you", "out")
+print(total)
+
+
+# Part 2 - dfs("dac", "fft") = 0
+
+fft = dfs("svr", "fft")
+dac = dfs("fft", "dac")
+out = dfs("dac", "out")
+total = fft * dac * out
+print(total)
